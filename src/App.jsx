79ff5032fa913f20cc9e06376f344e61cb931133
Fahim Muntasir <fahimmuntasir115@gmail.com>
2023-12-26 20:39:11 +0600

    Assignment done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Header from "./components/Header";
+import AddEntry from "./components/AddEntry";
+import Listing from "./components/Listing";
 
 function App() {
   return (
     <div>
       <Header />
       {/* <AddEntry /> */}
+      <AddEntry />
       {/* <Listing /> */}
+      <Listing />
     </div>
+
+    
   );
 }
 
--- src/components/Budget.jsx
+++ src/components/Budget.jsx
@@ -1,22 +1,19 @@
+import Available_budget from "./Available_budget";
+import Income from "./Income";
+import Expense from "./Expense";
+
 export default function Budget() {
   return (
     <div className="mx-auto max-w-sm px-5 py-8 text-center text-white">
-      <div>
-        <h2>Available Budget</h2>
-        <p className="mt-1 text-4xl font-medium">+ BDT 14,340.00</p>
-      </div>
+      {/* Available_budget */}
+      <Available_budget/>
+        
+      {/* Income */}
+      <Income />
 
-      <div className="mt-4 flex items-center justify-between bg-green-500 px-4 py-3 text-sm">
-        <p>Income</p>
-        <p>
-          + BDT <span id="total-income">3,391.45</span>
-        </p>
-      </div>
+      {/* Expense */}
+      <Expense />
 
-      <div className="mt-2 flex items-center justify-between bg-red-500 px-4 py-3 text-sm">
-        <span>Expenses</span>
-        <span>- BDT 1,762.69</span>
-      </div>
     </div>
   );
 }
